36d2f7ab52315f33e6b204dd291eafaf01d99e37
mengshouer <mengshouer@outlook.com>
2020-10-21 12:26:43 +0800

    update

--- NetEase_Music_daily/NetEase_Music_dailyForSCF.py
+++ NetEase_Music_daily/NetEase_Music_dailyForSCF.py
@@ -31,102 +31,106 @@ def protect(text):
     return {"params":encrypt('TA3YiYCfY2dDJQgg',encrypt('0CoJUm6Qyw8W8jud',text)),"encSecKey":"84ca47bca10bad09a6b04c5c927ef077d9b9f1e37098aa3eac6ea70eb59df0aa28b691b7e75e4f1f9831754919ea784c8f74fbfadf2898b0be17849fd656060162857830e241aba44991601f137624094c114ea8d17bce815b0cd4e5b8e2fbaba978c6d1d14dc3d1faf852bdd28818031ccdaaa13a6018e1024e2aae98844210"}
 
 def main(*args):
-    s=requests.Session()
-    header={}
-    url="https://music.163.com/weapi/login/cellphone"
-    url2="https://music.163.com/weapi/point/dailyTask"
-    url3="https://music.163.com/weapi/v1/discovery/recommend/resource"
-    logindata={
-        "phone":str(netease_username),
-        "countrycode":"86",
-        "password":md5(str(netease_password)),
-        "rememberLogin":"true",
-    }
-    headers = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-            "Referer" : "http://music.163.com/",
-            "Accept-Encoding" : "gzip, deflate",
-            }
-    headers2 = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-            "Referer" : "http://music.163.com/",
-            "Accept-Encoding" : "gzip, deflate",
-            "Cookie":"os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true;"
-            }
-
-    res=s.post(url=url,data=protect(json.dumps(logindata)),headers=headers2)
-    tempcookie=res.cookies
-    object=json.loads(res.text)
-    if object['code']==200:
-        print("登录成功！")
-    else:
-        print("登录失败！请检查密码是否正确！"+str(object['code']))
-
-    res=s.post(url=url2,data=protect('{"type":0}'),headers=headers)
-    object=json.loads(res.text)
-    if object['code']!=200 and object['code']!=-2:
-        print("签到时发生错误："+object['msg'])
-        if SCKEY:
-            scurl = f"https://sc.ftqq.com/{SCKEY}.send"
-            data = {
-                    "text" : "网易云音乐签到时发生错误",
-                    "desp" : object['msg']
-                    }
-            requests.post(scurl, data=data)
-    else:
-        if object['code']==200:
-            print("签到成功，经验+"+str(object['point']))
-        else:
-            print("重复签到")
-
-
-    res=s.post(url=url3,data=protect('{"csrf_token":"'+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']+'"}'),headers=headers)
-    object=json.loads(res.text,strict=False)
-    for x in object['recommend']:
-        url='https://music.163.com/weapi/v3/playlist/detail?csrf_token='+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']
-        data={
-            'id':x['id'],
-            'n':1000,
-            'csrf_token':requests.utils.dict_from_cookiejar(tempcookie)['__csrf'],
+    try:
+        s=requests.Session()
+        header={}
+        url="https://music.163.com/weapi/login/cellphone"
+        url2="https://music.163.com/weapi/point/dailyTask"
+        url3="https://music.163.com/weapi/v1/discovery/recommend/resource"
+        logindata={
+            "phone":str(netease_username),
+            "countrycode":"86",
+            "password":md5(str(netease_password)),
+            "rememberLogin":"true",
         }
-        res=s.post(url,protect(json.dumps(data)),headers=headers)
+        headers = {
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+                "Referer" : "http://music.163.com/",
+                "Accept-Encoding" : "gzip, deflate",
+                }
+        headers2 = {
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+                "Referer" : "http://music.163.com/",
+                "Accept-Encoding" : "gzip, deflate",
+                "Cookie":"os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true;"
+                }
+
+        res=s.post(url=url,data=protect(json.dumps(logindata)),headers=headers2)
+        tempcookie=res.cookies
+        object=json.loads(res.text)
+        if object['code']==200:
+            print("登录成功！")
+        else:
+            print("登录失败！请检查密码是否正确！"+str(object['code']))
+
+        res=s.post(url=url2,data=protect('{"type":0}'),headers=headers)
+        object=json.loads(res.text)
+        if object['code']!=200 and object['code']!=-2:
+            print("签到时发生错误："+object['msg'])
+            if SCKEY:
+                scurl = f"https://sc.ftqq.com/{SCKEY}.send"
+                data = {
+                        "text" : "网易云音乐签到时发生错误",
+                        "desp" : object['msg']
+                        }
+                requests.post(scurl, data=data)
+        else:
+            if object['code']==200:
+                print("签到成功，经验+"+str(object['point']))
+            else:
+                print("重复签到")
+
+
+        res=s.post(url=url3,data=protect('{"csrf_token":"'+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']+'"}'),headers=headers)
         object=json.loads(res.text,strict=False)
-        buffer=[]
-        count=0
-        for j in object['playlist']['trackIds']:
-            data2={}
-            data2["action"]="play"
-            data2["json"]={}
-            data2["json"]["download"]=0
-            data2["json"]["end"]="playend"
-            data2["json"]["id"]=j["id"]
-            data2["json"]["sourceId"]=""
-            data2["json"]["time"]="240"
-            data2["json"]["type"]="song"
-            data2["json"]["wifi"]=0
-            buffer.append(data2)
-            count+=1
+        for x in object['recommend']:
+            url='https://music.163.com/weapi/v3/playlist/detail?csrf_token='+requests.utils.dict_from_cookiejar(tempcookie)['__csrf']
+            data={
+                'id':x['id'],
+                'n':1000,
+                'csrf_token':requests.utils.dict_from_cookiejar(tempcookie)['__csrf'],
+            }
+            res=s.post(url,protect(json.dumps(data)),headers=headers)
+            object=json.loads(res.text,strict=False)
+            buffer=[]
+            count=0
+            for j in object['playlist']['trackIds']:
+                data2={}
+                data2["action"]="play"
+                data2["json"]={}
+                data2["json"]["download"]=0
+                data2["json"]["end"]="playend"
+                data2["json"]["id"]=j["id"]
+                data2["json"]["sourceId"]=""
+                data2["json"]["time"]="240"
+                data2["json"]["type"]="song"
+                data2["json"]["wifi"]=0
+                buffer.append(data2)
+                count+=1
+                if count>=310:
+                    break
             if count>=310:
                 break
-        if count>=310:
-            break
-    url = "http://music.163.com/weapi/feedback/weblog"
-    postdata={
-        "logs":json.dumps(buffer)
-    }
-    res=s.post(url,protect(json.dumps(postdata)))
-    object=json.loads(res.text,strict=False)
-    if object['code']==200:
-        print("刷单成功！共"+str(count)+"首")
-    else:
-        print("发生错误："+str(object['code'])+object['message'])
-        if SCKEY:
-            scurl = f"https://sc.ftqq.com/{SCKEY}.send"
-            data = {
-                    "text" : "网易云音乐刷歌单时发生错误",
-                    "desp" : object['message']
-                    }
-            requests.post(scurl, data=data)
+        url = "http://music.163.com/weapi/feedback/weblog"
+        postdata={
+            "logs":json.dumps(buffer)
+        }
+        res=s.post(url,protect(json.dumps(postdata)))
+        object=json.loads(res.text,strict=False)
+        if object['code']==200:
+            print("刷单成功！共"+str(count)+"首")
+        else:
+            print("发生错误："+str(object['code'])+object['message'])
+            if SCKEY:
+                scurl = f"https://sc.ftqq.com/{SCKEY}.send"
+                data = {
+                        "text" : "网易云音乐刷歌单时发生错误",
+                        "desp" : object['message']
+                        }
+                requests.post(scurl, data=data)
+    except Exception as e:
+        print('repr(e):', repr(e))
+
 
 if __name__ == "__main__":
     main()
