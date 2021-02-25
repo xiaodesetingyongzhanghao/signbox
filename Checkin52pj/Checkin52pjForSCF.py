aacb6e69eb3d8bdb582b601bcd3ea144942923e1
mengshouer <mengshouer@outlook.com>
2021-02-25 22:04:55 +0800

    pusher add pushplus

--- Checkin52pj/Checkin52pjForSCF.py
+++ Checkin52pj/Checkin52pjForSCF.py
@@ -15,6 +15,8 @@ def pusher(*args):
     SCTKEY = os.environ.get('SCTKEY') # http://sct.ftqq.com/
     Skey = os.environ.get('Skey') # https://cp.xuthus.cc/
     Smode = os.environ.get('Smode') # send, group, psend, pgroup, wx, tg, ww, ding(no send email)
+    pushplus_token = os.environ.get('pushplus_token') # http://pushplus.hxtrip.com/
+    pushplus_topic = os.environ.get('pushplus_topic') # pushplus一对多推送需要的"群组编码"，一对一推送不用管
     if SCKEY:
         sendurl = f"https://sc.ftqq.com/{SCKEY}.send"
         data = {
@@ -29,6 +31,29 @@ def pusher(*args):
             "desp" : othermsg
             }
         requests.post(sendurl, data=data)
+    if pushplus_token:
+        sendurl = f"http://pushplus.hxtrip.com/send"
+        if not othermsg:
+            othermsg = msg
+        if pushplus_topic:
+            params = {
+            "token" : pushplus_token,
+            "title" : msg,
+            "content" : othermsg,
+            "template" : "html",
+            "topic" : pushplus_topic
+            }
+        else:
+            params = {
+                "token" : pushplus_token,
+                "title" : msg,
+                "content" : othermsg,
+                "template" : "html"
+            }
+        r = requests.post(sendurl, params=params)
+        print(r.json())
+        if r.json()["code"] != 200:
+            print(f"pushplus推送失败！{r.json()['msg']}")
     if Skey:
         if not Smode:
             Smode = 'send'
